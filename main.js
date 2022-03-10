4401a220a3e310c37f37f08d71ad071475ac8ed0
carlos <guzmancarlos797@gmail.com>
2022-03-09 21:12:24 -0300

    Imersão Dev alura-aula01

---
+++ main.js
@@ -0,0 +1,13 @@
+var nome = "Carlos";
+var n1 = 11;
+var n2 = 9;
+var n3 = 8;
+var n4 = 7.5;
+var n5 = 6;
+//var media = (n1erBim + n2erBim + n3erBim + n4erBim + n5erBim)/5;
+//var mediaF = media.toFixed(3);
+console.log(`Seja bem vindo, ${nome}. Sua nota média é: ${((n1+n2+n3+n4+n5)/5).toFixed(0)}`);
+
+//let num = 5.56789;
+//let n = num.toFixed(10);
+//console.log(n);
